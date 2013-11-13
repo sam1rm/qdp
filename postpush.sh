6096fef61faaa2490af65c9f70cbc96d2dc74d9a
Glenn Sugden <headcrash@berkeley.edu>
2013-11-13 14:30:57 -0800

    Moved version output to prepush

--- postpush.sh
+++ postpush.sh
@@ -6,5 +6,4 @@
 #pycco db_repository/*.py
 #git push origin master
 git push heroku master
-heroku config:add rel=$(heroku releases | tail -2 | awk '{print $5"t"$6$7"."$1}')
 open http://qdp.herokuapp.com
