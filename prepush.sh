363d5a69870c22ce508aba379b5126145cd89106
Glenn Sugden <headcrash@berkeley.edu>
2013-11-13 14:30:29 -0800

    Moved version output to prepush

--- prepush.sh
+++ prepush.sh
@@ -5,5 +5,6 @@ pycco *.py
 pycco app/*.py
 pycco db_repository/*.py
 git push origin master
+heroku releases | tail -2 | awk '{print $5"t"$6$7"."$1}' > version.txt
 open -a "GitHub"
 #git push heroku master
